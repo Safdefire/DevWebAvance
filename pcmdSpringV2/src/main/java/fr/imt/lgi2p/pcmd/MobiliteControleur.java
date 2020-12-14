2845cf306b2ae92e4c4fa62b13d59954f2547249
Safdefire <56444953+Safdefire@users.noreply.github.com>
2020-12-14 17:57:44 +0100

    Récup branche Samah

---
+++ pcmdSpringV2/src/main/java/fr/imt/lgi2p/pcmd/MobiliteControleur.java
@@ -0,0 +1,45 @@
+package fr.imt.lgi2p.pcmd;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+
+
+
+@RestController
+public class MobiliteControleur {
+	@Autowired
+	private MobiliteDao mobiliteDao;
+	
+	@RequestMapping(value="/mobilites", method=RequestMethod.GET)
+	   public List<Mobilite> listeMobilites() {
+		return mobiliteDao.listMobilite();
+	}
+	@PostMapping(value = "/ajouterMobilite" )
+	public Mobilite ajoutMobilite(@RequestBody String mobilite)
+		{
+		ObjectMapper mapper = new ObjectMapper();
+	     // Petit bug qui permet de forcer le passage d'un int en long
+	     mapper.configure(DeserializationFeature.USE_LONG_FOR_INTS, true);
+	     try {
+	    	 Mobilite newMobilite = mapper.readValue(mobilite, Mobilite.class);
+	    	 Mobilite resultMobilite = mobiliteDao.ajouterMobilite(newMobilite);
+	         return ( resultMobilite );
+	     } catch (JsonProcessingException e) {
+	        e.printStackTrace();
+	     }
+	     return (null);
+			
+		}
+	
+}
